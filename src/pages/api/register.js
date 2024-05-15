3f066893e5816d2f0982b8352f7ae28d106fcdeb
Divyansh Dixir <divyanshdixit96@gmail.com>
2024-05-15 16:51:16 +0530

    file path chage

--- src/pages/api/register.js
+++ src/pages/api/register.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../prisma/generated/client/edge'
+import { PrismaClient } from '../../../prisma/generated/client'
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
 
